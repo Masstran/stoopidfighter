b8126657f0ef85711da0869dc1100fdf2dc0cd46
Masstran <15689794+Masstran@users.noreply.github.com>
2024-10-20 14:34:25 +0300

    GODOT-14: add health text above healthbar

--- fighter.gd
+++ fighter.gd
@@ -2,16 +2,17 @@ extends Node2D
 
 class_name Fighter
 
+enum DIRECTION {LEFT, RIGHT}
 @export var health = 100
 @export var strength = 10
+@export var direction = DIRECTION.RIGHT
 var enemy : Fighter
 signal death
+signal change_direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = 100
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
