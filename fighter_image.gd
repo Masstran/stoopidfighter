586d62a8b369fbb90a0ce564a8923337965e7a50
ArtemShendyapin <artemij.shendyapin@yandex.ru>
2024-10-26 13:48:35 +0300

    Resolve conflicts

--- fighter_image.gd
+++ fighter_image.gd
@@ -1,17 +1,12 @@
-extends ColorRect
+extends Sprite2D
 
-var max_health
-var base_node: Fighter
-var max_size
+var base_node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	base_node = get_parent().get_parent()
-	max_health = base_node.health
-	max_size = size.x
+	base_node = get_parent()
 	if base_node.direction == base_node.DIRECTION.LEFT:
 		scale.x *= -1
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	size.x = max_size * base_node.health / max_health
+	pass
--- health_text.gd
+++ health_text.gd
@@ -1,17 +1,16 @@
-extends ColorRect
+extends RichTextLabel
 
-var max_health
 var base_node: Fighter
-var max_size
+var max_health: int
+var formatted_text = "%s/%s"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	base_node = get_parent().get_parent()
 	max_health = base_node.health
-	max_size = size.x
-	if base_node.direction == base_node.DIRECTION.LEFT:
-		scale.x *= -1
+	text = formatted_text % [max_health, max_health]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	size.x = max_size * base_node.health / max_health
+	text = formatted_text % [base_node.health, max_health]
